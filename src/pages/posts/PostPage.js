adb911de0f54e631d03bae572feb35d2e87fe0c4
Elvira Tidemo <elvir@MacBook-Air.local>
2024-03-27 20:33:18 +0100

    Update PostPage.js

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -22,15 +22,14 @@ function PostPage() {
   useEffect(() => {
     const handleMount = async () => {
       try {
-        const [{ data: post }, {data: comments}] = await Promise.all([
+        const [{ data: post }, { data: comments }] = await Promise.all([
           axiosReq.get(`/posts/${id}`),
-          axiosReq.get(`/comments/?post=${id}`)
-        ])
-        setPost({ results: [post] })
-        setComments(comments)
-        console.log(post)
+          axiosReq.get(`/comments/?post=${id}`),
+        ]);
+        setPost({ results: [post] });
+        setComments(comments);
       } catch (err) {
-        console.log(err)
+        console.log(err);
       }
     };
 
@@ -55,10 +54,13 @@ function PostPage() {
             "Comments"
           ) : null}
           {comments.results.length ? (
-            comments.results.map(comment => (
-              <p key={comment.id}>
-                {comment.owner}: {comment.content}
-              </p>
+            comments.results.map((comment) => (
+              <Comment
+                key={comment.id}
+                {...comment}
+                setPost={setPost}
+                setComments={setComments}
+              />
             ))
           ) : currentUser ? (
             <span>No comments yet, be the first to comment!</span>
