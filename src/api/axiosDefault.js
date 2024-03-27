28abe3fb59ff80a4e5f30a4309bb00d0bfa26d5a
Elvira Tidemo <elvir@MacBook-Air.local>
2024-03-27 08:33:14 +0100

    Install packages of older versions for compatibility

--- src/api/axiosDefault.js
+++ src/api/axiosDefault.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://world-of-craft-0e06bf8581a1.herokuapp.com/';
+axios.defaults.baseURL = 'https://world-of-craft-670e0fb14b24.herokuapp.com/';
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
 
 export const axiosReq = axios.create();
