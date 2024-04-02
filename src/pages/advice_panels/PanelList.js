59a7f08066ae5737dc06df8c4ae270a4dcd67c76
Elvira Tidemo <elvir@MacBook-Air.local>
2024-04-02 10:48:40 +0200

    Add styling to panelList

--- src/pages/advice_panels/PanelList.js
+++ src/pages/advice_panels/PanelList.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import PanelDetail from './PanelDetail';
 import { Link } from 'react-router-dom';
+import { Container, Row, Col } from 'react-bootstrap';
 
 function PanelList() {
     const [panels, setPanels] = useState([]);
@@ -19,24 +20,31 @@ function PanelList() {
     }, []);
 
     const handlePanelClick = (panelId) => {
-        console.log('Panel ID clicked:', panelId)
         setSelectedPanelId(panelId);
     };
-    console.log('Panels:', panels);
 
     return (
-        <div>
+        <Container className="text-center">
             <h2>Admin Advice Panels</h2>
-            <ul>
-                {panels.map(panel => (
-                    <li key={panel.id} onClick={() => handlePanelClick(panel.id)}>
-                        <Link to={`/panels/${panel.id}`}>{panel.title}</Link>
-                    </li>
-                ))}
-            </ul>
+            <br/>
+            <br/>
+            <Row>
+                <Col md={{ span: 6, offset: 3 }}>
+                    {panels.map(panel => (
+                        <div key={panel.id} onClick={() => handlePanelClick(panel.id)} style={{ marginBottom: '15px' }}>
+                            <Link to={`/panels/${panel.id}`} className="text-decoration-none">
+                                <div className="border rounded p-3 d-flex align-items-center">
+                                    {panel.image && <img src={panel.image} alt="Panel thumbnail" style={{ width: '50px', height: '50px', marginRight: '10px' }} />}
+                                    <h4 style={{ marginLeft: '10px' }}>{panel.title}</h4>
+                                </div>
+                            </Link>
+                        </div>
+                    ))}
+                </Col>
+            </Row>
             {selectedPanelId && <PanelDetail panelId={selectedPanelId} />}
-        </div>
+        </Container>
     );
 }
 
-export default PanelList
+export default PanelList;
